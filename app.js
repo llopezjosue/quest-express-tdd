2e476ce7959f476f02e3dad0f04bde0c2c13079d
Josué Llopez <llopezjosue31@gmail.com>
2020-06-09 11:02:27 +0200

    premiers tests ok

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+require('dotenv').config();
+const connection = require('./connection');
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Hello World!" });
+  });
+
 module.exports = app;

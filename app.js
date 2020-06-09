284f54118cac98c2912814422f9614d5a8fcb14f
Josué Llopez <llopezjosue31@gmail.com>
2020-06-09 13:58:00 +0200

    finalisation des tests ok

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 require('dotenv').config();
 const connection = require('./connection');
+
 
 
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: "Hello World!" });
   });
+
+  app.post('/bookmarks', (req, res) => {
+    const { url, title } = req.body;
+    if (!url || !title) {
+      return res.status(422).json({ error: 'required field(s) missing' });
+    }
+    connection.query('INSERT INTO bookmark SET ?', req.body, (err, stats) => {
+        if (err) return res.status(500).json({ error: err.message, sql: err.sql });
+    
+        connection.query('SELECT * FROM bookmark WHERE id = ?', stats.insertId, (err, records) => {
+          if (err) return res.status(500).json({ error: err.message, sql: err.sql });
+          return res.status(201).json(records[0]);
+        });
+      });
+    });
+
+
+    app.get('/bookmark/:id', (req, res) => {
+        const idBookmark = req.params.id;
+        connection.query('SELECT * FROM bookmark WHERE id = ?', [idBookmark], (err, results) => {
+            if(err){
+                res.status(500).json({
+                    error: err.message,
+                    sql: err.sql
+                });
+            }
+            else if(results.length === 0){
+                res.status(404).json({ "error": "Bookmark not found" });
+            }
+            else{
+                res.status(200).json(results[0]);
+            }
+        });
+    });
+    
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
+beforeEach(done => connection.query('TRUNCATE bookmark', done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -13,4 +15,67 @@ describe('Test routes', () => {
         done();
       });
   });
+
+  it('POST /bookmarks champs manquant', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = {"error": "required field(s) missing"};
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  it('POST /bookmarks champs ok', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: expect.any(Number), url: 'https://jestjs.io', title: 'Jest' };
+
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  });
+});
+
+describe('GET /bookmark/:id', () => {
+    const testBookmark = { url: 'https://jestjs.io', title: 'Jest' };
+    beforeEach((done) => connection.query(
+        'TRUNCATE bookmark', () => connection.query(
+            'INSERT INTO bookmark SET ?', testBookmark, done
+        )
+    ));
+
+    it('GET / NO - bookmark introuvable', (done) => {
+        request(app)
+        .get('/bookmark/:id')
+        .send({})
+        .expect(404)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            const expected = { error: 'Bookmark not found' };
+            expect(response.body).toEqual(expected);
+            done();
+        });
+    });
+
+    it('GET / OK - bookmark trouvé', (done) => {
+        request(app)
+        .get('/bookmark/1')
+        .send(testBookmark)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            const expected = {  id: 1, url: 'https://jestjs.io', title: 'Jest' };
+            expect(response.body).toEqual(expected);
+            done();
+        });
+    });
 });
